28a62e72c026be0726b10018bbf469f326a84757
Alex Zernov <it_live@bk.ru>
2022-01-21 10:52:10 +0300

    my_first_commit

---
+++ Change_res_aws.tf
@@ -0,0 +1,17 @@
+provider "aws" {        #connect in provider with using access_key and secret key. Check region where avialibiliaty zone
+    access_key = "t4rtyryrty5645"
+    secret_key = " rtyhrhyhtyhyth"
+    region = "eu-central-1"
+}
+
+resource "aws-instance" "my_ubuntu" { #install ubuntu from image ami ubuntu with check type instance t3.micro
+    ami = ""
+    count = 3
+    instance-type "t3.small"###change type at t3.small
+}
+
+resource "aws-instance" "amazon-l" { #install ubuntu from image ami ubuntu with check type instance t3.micro
+    ami = "ami-jdncj333"
+    instance-type "t3.micro"
+
+}
